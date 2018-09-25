5922381c61b7b6f39f71f42d7e958e1ec5c81640
garyDav <gary.2810.dav@gmail.com>
2018-09-24 20:36:53 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { TranslateModule } from '@ngx-translate/core';
+import 'hammerjs';
+
+import { FuseModule } from '@fuse/fuse.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseProgressBarModule, FuseSidebarModule, FuseThemeOptionsModule } from '@fuse/components';
+
+import { fuseConfig } from 'app/fuse-config';
+
+import { AppComponent } from 'app/app.component';
+import { LayoutModule } from 'app/layout/layout.module';
+import { SampleModule } from 'app/main/sample/sample.module';
+
+import { CashierComponent } from './cashier/cashier.component';
+import { CookComponent } from './cook/cook.component';
+import { WaiterComponent } from './waiter/waiter.component';
+
+const appRoutes: Routes = [
+    // {
+    //     path      : '**',
+    //     redirectTo: 'sample'
+    // },
+    { path: '', redirectTo: '/mesero', pathMatch: 'full' },
+    { path: 'cajero', component: CashierComponent },
+    { path: 'mesero', component: WaiterComponent },
+    { path: 'cocina', component: CookComponent }
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CashierComponent,
+        CookComponent,
+        WaiterComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+
+        TranslateModule.forRoot(),
+
+        // Material moment date module
+        MatMomentDateModule,
+
+        // Material
+        MatButtonModule,
+        MatIconModule,
+
+        // Fuse modules
+        FuseModule.forRoot(fuseConfig),
+        FuseProgressBarModule,
+        FuseSharedModule,
+        FuseSidebarModule,
+        FuseThemeOptionsModule,
+
+        // App modules
+        LayoutModule,
+        SampleModule
+    ],
+    bootstrap   : [
+        AppComponent
+    ]
+})
+export class AppModule
+{
+}
